980a38f3a148a49ab5dfff4715d8afffc0b3efd4
OlaAyman <olaAyman@example.com>
2021-09-27 22:28:44 +0200

    final version

--- server.js
+++ server.js
@@ -2,18 +2,43 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+function serverListener(){
+    console.log(`server is running on port : ${port}`);
+}
+const server = app.listen(port,serverListener);
+
+// post function
+let data = []
+app.post('/post',function(req,res){
+    
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.user_response = req.body.user_response;
+    
+    let date1 = new Date();
+    console.log(date1);
+});
+
+// get function
+app.get('/update',(req,res)=>{
+    res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,80 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
+let apiKey = 'ea95ac7f7f17f9b713074d3efed22cf7';
+let UserResponse = '';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//event listener for the generate button
+document.getElementById('generate').addEventListener('click', onGenerateClick);
+
+// callback function for the button click
+function onGenerateClick(e){
+    //get the values og the user entry as the zip id for the API and the feeling about the weather
+    UserResponse =  document.getElementById('feelings').value;
+    const zipId = document.getElementById('zip').value;
+
+    //call the async function to Acquire API credentials from OpenWeatherMap website 
+    getTheWeather(baseURL , zipId,apiKey)
+    .then(function(weatherData){
+        const temp = weatherData.list[0].main.temp; 
+        // post the weatherdata to the server by chainning the promises
+        postData('/post', { temperature: temp , date : newDate , user_response : UserResponse });
+        //update the UI with the new data
+        updateUI();
+    })
+
+}
+
+//async function to to Acquire API credentials from OpenWeatherMap website
+async function getTheWeather(baseURL , zipId , key){
+
+  const res = await fetch(baseURL + zipId + key);
+  try {
+    const weatherData = await res.json();
+    return weatherData ;
+
+  }  catch(error) {
+    console.log("error", error);
+  }
+}
+
+//async function to post the data to the server
+async function postData(url = '', data = {}) {
+    
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  //async function to update  the UI
+  async function updateUI(){
+
+    const res = await fetch('/update');
+    try {
+      const UIData = await res.json();
+      console.log(UIData);
+      //set the HTML text in the index file with the data retrived from the server
+      document.getElementById('date').innerHTML = "the date is:  " + UIData.date;
+      document.getElementById('temp').innerHTML = "the temperature is:  " + UIData.temperature;
+      document.getElementById('content').innerHTML = "the user feeling is:  " + UIData.user_response;
+      return UIData ;
+  
+    }  catch(error) {
+      console.log("error", error);
+    }
+  }
+  
